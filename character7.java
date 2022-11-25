b010ffe37167934cc8c3b3e6aa2199851d91f02a
Ayazaligorchani <117918571+Ayazaligorchani@users.noreply.github.com>
2022-11-25 19:57:14 +0500

    Add files via upload
    Work Done

---
+++ character7.java
@@ -0,0 +1,11 @@
+class character7{
+public static void main(String[]arg){
+System.out.println("\"The\"\"Hellow\" \"World\"");
+System.out.println("Nice to meet you\\");
+System.out.println("Hello World");
+System.out.println("Nice to meet you\\");
+System.out.println("\nI ");
+System.out.println("\twant \n");
+System.out.println("\t\t to learn\n");
+System.out.println("\t\t\t java");
+}}
